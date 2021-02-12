0730495e7fa16ddcb258e56e9c7a31a66c154060
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-12 19:20:50 +0100

    ng- directives
    ng-app
    ng-model
    ng-bind

--- angularJS01.js
+++ angularJS01.js
@@ -8,3 +8,25 @@
   <p ng-bind="name"></p>
 </div>
 */
+
+// ng-app directive tells AngularJS that <div> element is the "owner" of  Angular app
+// ng-model directive binds the value of the input field to the app variable - name
+// ng-bind directive binds the content of the <p> element to the app
+
+// Angular JS expressions are written inside double braces: {{ expression}}.
+/*
+<div ng-app="">
+  <p>My first expression: {{ 5 + 5 }}</p>
+</div>
+*/
+
+// another example
+/*
+<div ng-app="">
+ 
+ <p>Input something in the input box:</p>
+ <p>Name: <input type="text" ng-model="name"></p>
+ <p>{{name}}</p>
+ 
+ </div>
+ */
