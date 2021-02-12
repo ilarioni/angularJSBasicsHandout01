4476f511dc4ccb14676293ccdee070fa06346c2a
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-12 19:20:12 +0100

    intro

--- angularJS01.js
+++ angularJS01.js
@@ -1,1 +1,10 @@
 // Intro to AngularJS
+// AngularJS extends HTML with new attributes.
+// Angular JS is perfect for Single Page Applications(SPAs)
+
+/*
+<div ng-app="">
+  <p>Name: <input type="text" ng-model="name"></p>
+  <p ng-bind="name"></p>
+</div>
+*/
