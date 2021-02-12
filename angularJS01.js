4927b3299ec5b21fdf1e1320d44a2bcbc1f24006
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-12 19:25:24 +0100

    ng-model directive

--- angularJS01.js
+++ angularJS01.js
@@ -99,3 +99,15 @@
 
 </div>
 */
+// ng-model directive
+/*
+<div ng-app="myApp" ng-controller="myCtrl">
+  Name: <input ng-model="name">
+</div>
+*/
+<script>
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+  $scope.name = "John Doe";
+});
+</script>
